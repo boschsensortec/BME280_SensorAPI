b6d402ff6a7df160acc7a41ad9c6be4f88de2e92
Laura Lindzey <lindzey@uw.edu>
2023-05-07 06:40:47 +0000

    Always use ioctl to set device address

--- examples/contributed/linux_userspace.c
+++ examples/contributed/linux_userspace.c
@@ -20,10 +20,8 @@
  * \include linux_userspace.c
  */
 
-#ifdef __KERNEL__
 #include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
-#endif
 
 /******************************************************************************/
 /*!                         System header files                               */
@@ -154,17 +152,14 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
-#ifdef __KERNEL__
+    /* Make sure to select BME280_I2C_ADDR_PRIM or BME280_I2C_ADDR_SEC as needed */
+    id.dev_addr = BME280_I2C_ADDR_PRIM;
+
     if (ioctl(id.fd, I2C_SLAVE, id.dev_addr) < 0)
     {
         fprintf(stderr, "Failed to acquire bus access and/or talk to slave.\n");
         exit(1);
     }
-
-#endif
-
-    /* Make sure to select BME280_I2C_ADDR_PRIM or BME280_I2C_ADDR_SEC as needed */
-    id.dev_addr = BME280_I2C_ADDR_PRIM;
 
     dev.intf = BME280_I2C_INTF;
     dev.read = user_i2c_read;
